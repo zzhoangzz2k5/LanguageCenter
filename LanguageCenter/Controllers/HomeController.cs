566fa403299953a42b6dce081ad7d29bf014c735
zzhoangzz2k5 <shoang450@gmail.com>
2026-06-03 10:46:45 +0700

    Add project files.

---
+++ LanguageCenter/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using LanguageCenter.Models;
+using System.Configuration;
+using System.Linq;
+using System.Web.Mvc;
+
+public class HomeController : Controller
+{
+    LanguageCenterDataContext db =
+        new LanguageCenterDataContext(
+            ConfigurationManager.ConnectionStrings["LanguageCenterConnectionString"].ConnectionString);
+
+    public ActionResult Index()
+    {
+        var programs = db.Programs.ToList();
+        return View(programs);
+    }
+}
